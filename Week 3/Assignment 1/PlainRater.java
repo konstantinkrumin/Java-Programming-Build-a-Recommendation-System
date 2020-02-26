b52511e08c1c3743749fcb198388be20ea209245
Konstantin Krumin <konstantin.krumin@gmail.com>
2020-02-27 00:46:51 +0300

    Add files via upload

---
+++ Week 3/Assignment 1/PlainRater.java
@@ -0,0 +1,58 @@
+
+/**
+ * PlainRater class contains various methods that are used to add and extract ratings information.
+ * 
+ */
+
+import java.util.*;
+
+public class PlainRater implements Rater {
+    private String myID;
+    private ArrayList<Rating> myRatings;
+
+    public PlainRater(String id) {
+        myID = id;
+        myRatings = new ArrayList<Rating>();
+    }
+
+    public void addRating(String item, double rating) {
+        myRatings.add(new Rating(item,rating));
+    }
+
+    public boolean hasRating(String item) {
+        for(int k=0; k < myRatings.size(); k++){
+            if (myRatings.get(k).getItem().equals(item)){
+                return true;
+            }
+        }
+        
+        return false;
+    }
+
+    public String getID() {
+        return myID;
+    }
+
+    public double getRating(String item) {
+        for(int k=0; k < myRatings.size(); k++){
+            if (myRatings.get(k).getItem().equals(item)){
+                return myRatings.get(k).getValue();
+            }
+        }
+        
+        return -1;
+    }
+
+    public int numRatings() {
+        return myRatings.size();
+    }
+
+    public ArrayList<String> getItemsRated() {
+        ArrayList<String> list = new ArrayList<String>();
+        for(int k=0; k < myRatings.size(); k++){
+            list.add(myRatings.get(k).getItem());
+        }
+        
+        return list;
+    }
+}
